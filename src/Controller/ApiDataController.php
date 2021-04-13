d833fb58921f4fb7d1c7a0e6813e6e0e058cf5cd
KartikWatts <arorakartik4@gmail.com>
2021-04-13 22:06:35 +0530

    Checking origin headers

--- src/Controller/ApiDataController.php
+++ src/Controller/ApiDataController.php
@@ -54,6 +54,17 @@ class ApiDataController extends AbstractController
             array_push($data, $arr);
         }
 
+        if (array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+            $origin = $_SERVER['HTTP_ORIGIN'];
+        }
+        else if (array_key_exists('HTTP_REFERER', $_SERVER)) {
+            $origin = $_SERVER['HTTP_REFERER'];
+        } else {
+            $origin = $_SERVER['REMOTE_ADDR'];
+        }
+
+        die($origin);
+
         return new JsonResponse($data, Response::HTTP_OK);
     }
 
